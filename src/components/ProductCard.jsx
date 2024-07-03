b7df1c0842d98003328ab6c61e09a1416632681c
GarryAma <amasiagarry23@gmail.com>
2024-07-04 00:37:08 +1000

    toggle remove to cart and add functionality to + & - button(quantity)

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "./ui/button";
 import { IoIosAdd, IoIosRemove } from "react-icons/io";
+import { TestButton } from "./TestButton";
 
 export const ProductCard = ({ image, name, price, stock }) => {
+  const [quantity, setQuantity] = useState(0);
+  const [message, setMessage] = useState("add to cart");
+  const [isAdded, setIsAdded] = useState(true);
+
+  const increaseQuantity = () => {
+    if (quantity < stock) setQuantity((prev) => prev + 1);
+    return;
+  };
+
+  const decreaseQuantity = () => {
+    if (quantity > 0) setQuantity((prev) => prev - 1);
+    return;
+  };
+
   return (
     <div className="p-4 border rounded-md md:max-w-96 flex flex-col gap-4">
       <div className="aspect-square w-full overflow-hidden rounded-md">
@@ -18,18 +33,33 @@ export const ProductCard = ({ image, name, price, stock }) => {
       <div className="flex flex-col gap-4">
         {/* button quantity */}
         <div className="flex justify-between items-center">
-          <Button size="icon" variant="outline">
+          <Button
+            disabled={quantity <= 0}
+            onClick={decreaseQuantity}
+            size="icon"
+            variant="outline"
+          >
             <IoIosRemove className="h-6 w-6" />
           </Button>
-          <p className="font-bold text-lg">0</p>
-          <Button size="icon" variant="outline">
+          <p className="font-bold text-lg">{quantity}</p>
+          <Button
+            disabled={quantity >= stock}
+            onClick={increaseQuantity}
+            size="icon"
+            variant="outline"
+          >
             <IoIosAdd className="h-6 w-6" />
           </Button>
         </div>
 
         {/* button ad to cart */}
         <div>
-          <Button className="w-full">add to cart</Button>
+          <Button
+            onClick={() => setIsAdded((prev) => !prev)}
+            className="w-full"
+          >
+            {isAdded === true ? "remove from cart" : "add to cart"}
+          </Button>
         </div>
       </div>
     </div>
