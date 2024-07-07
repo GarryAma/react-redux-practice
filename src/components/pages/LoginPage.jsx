bbfe20b717014cfe3c7bbde4e0df2e46f5350673
GarryAma <amasiagarry23@gmail.com>
2024-07-08 06:51:43 +1000

    adding feature hide and show password

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Card,
   CardContent,
@@ -12,6 +12,14 @@ import { Checkbox } from "../ui/checkbox";
 import { Button } from "../ui/button";
 
 export const LoginPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isChecked, setIsCheched] = useState(false);
+  const [errorUsernameMessage, setErrorUsernameMassage] = useState("");
+  const [errorPasswordMessage, setErrorPasswordMassage] = useState("");
+
+  const handleOnSubmit = () => {};
+
   return (
     <div className="px-4 container py-8 flex flex-col justify-center items-center max-w-screen-md h-[90vh]">
       <Card className="w-full max-w-[540px]">
@@ -20,21 +28,36 @@ export const LoginPage = () => {
         </CardHeader>
         <CardContent className="flex flex-col gap-4">
           <div>
-            <Label>Username : </Label>
-            <Input />
+            <Label htmlFor="username">Username : </Label>
+            <Input
+              onChange={(event) => {
+                setUsername(event.target.value);
+              }}
+              id="username"
+            />
           </div>
           <div>
-            <Label>Password : </Label>
-            <Input type="password" />
+            <Label htmlFor="password">Password : </Label>
+            <Input
+              onChange={(event) => setPassword(event.target.value)}
+              type={isChecked ? "text" : "password"}
+              id="password"
+            />
+            <p className="text-xs text-gray-500">{errorPasswordMessage}</p>
           </div>
           <div className="flex items-center space-x-2">
             <Label htmlFor="show password">Show Password:</Label>
-            <Checkbox id="show password" />
+            <Checkbox
+              id="show password"
+              onCheckedChange={(current) => {
+                setIsCheched(current);
+              }}
+            />
           </div>
         </CardContent>
         <CardFooter>
           <div className="flex flex-col space-y-4 w-full">
-            <Button>Login</Button>
+            <Button onClick={handleOnSubmit}>Login</Button>
             <Button
               variant="link"
               className="hover:font-light hover:text-green-900"
