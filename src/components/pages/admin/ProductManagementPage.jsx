3b7ff2cc9356c3b27e10cfed4c3db8792efb4232
GarryAma <amasiagarry23@gmail.com>
2024-07-16 03:35:56 +1000

    adding pagination done,successfully implemented next and previous button and refetch the data based on _per_page & _page

--- src/components/pages/admin/ProductManagementPage.jsx
+++ src/components/pages/admin/ProductManagementPage.jsx
@@ -1,6 +1,11 @@
 import { AdminLayout } from "@/components/layout/AdminLayout";
 import { Button } from "@/components/ui/button";
 import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+} from "@/components/ui/pagination";
+import {
   Table,
   TableBody,
   TableCell,
@@ -9,23 +14,45 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { axiosInstance } from "@/lib/axios";
-import { Ellipsis } from "lucide-react";
+import { ChevronLeft, ChevronRight, Ellipsis } from "lucide-react";
 import React, { useEffect, useState } from "react";
 import { IoAdd } from "react-icons/io5";
+import { useSearchParams } from "react-router-dom";
 
 export const ProductManagementPage = () => {
   const [products, setProducts] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams({ halaman: 1 });
+  console.log(searchParams.get("halaman"));
+
+  // console.log("this render");
+
+  const handleNextPage = () => {
+    searchParams.set("halaman", Number(searchParams.get("halaman")) + 1);
+    setSearchParams(searchParams);
+  };
+
+  const handlePreviousPage = () => {
+    if (Number(searchParams.get("halaman")) > 1) {
+      searchParams.set("halaman", Number(searchParams.get("halaman")) - 1);
+      setSearchParams(searchParams);
+    } else {
+      alert("sudah ada di halaman 1");
+    }
+  };
 
   const fetchProducts = async () => {
     try {
       // const response = await axiosInstance.get("/products?_limit=5");
       const response = await axiosInstance.get("/products", {
+        //we can use config params:{} ->given by axios to handle query params
         params: {
-          _limit: 5,
-          // _page: 1,
+          // _limit: 5, =>if we use _page we're not allowed to use _limit anymore(json-server), we must use _per_page(json-server)
+          _per_page: 5,
+          _page: Number(searchParams.get("halaman")),
         },
       });
-      setProducts(response.data);
+      // console.log(response.data);
+      setProducts(response.data.data);
     } catch (error) {
       console.log(error.message);
     }
@@ -34,7 +61,7 @@ export const ProductManagementPage = () => {
   useEffect(() => {
     // console.log("di useEffect jalan");
     fetchProducts();
-  }, []);
+  }, [searchParams.get("halaman")]);
 
   return (
     <>
@@ -65,12 +92,12 @@ export const ProductManagementPage = () => {
           <TableBody>
             {/* {console.log(products)}
             {console.log(products)} */}
-            {products.map((singleProduct) => {
+            {products.map((singleProduct, i) => {
               // console.log(singleProduct);
               const { id, name, price, stock } = singleProduct;
               // console.log(id);
               return (
-                <TableRow>
+                <TableRow key={i}>
                   <TableCell>{id}</TableCell>
                   <TableCell>{name}</TableCell>
                   <TableCell>{price}</TableCell>
@@ -85,7 +112,29 @@ export const ProductManagementPage = () => {
             })}
           </TableBody>
         </Table>
+
+        <Pagination className={"mt-8"}>
+          <PaginationContent>
+            <PaginationItem>
+              <Button onClick={handlePreviousPage} variant="ghost">
+                <ChevronLeft className="w-4 h-4 mr-2" />
+                Previous
+              </Button>
+            </PaginationItem>
+            <PaginationItem className="mx-8 font-semibold text-xs">
+              Page {searchParams.get("halaman")}
+            </PaginationItem>
+            <PaginationItem>
+              <Button onClick={handleNextPage} variant="ghost">
+                Next
+                <ChevronRight className="w-4 h-4 ml-2" />
+              </Button>
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
       </AdminLayout>
     </>
   );
 };
+
+//why do we use useSearchParams?because it persist the state,so when we refresh, it will stay on the current page and it doenst reset state!!unlike useState,it will reset the state so it will come back to initial state!!
